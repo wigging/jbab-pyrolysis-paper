cde359afb83ebd997fba8962a5dd41493ee03a99
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2020-08-31 17:22:37 -0400

    Compare gas mixture viscosity data

--- code/funcs.py
+++ code/funcs.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def biot(h, r, k):
     """
@@ -71,3 +73,40 @@ def pyro2(h, kr, rho, cp, r):
     """
     py = h / (kr * rho * cp * r)
     return py
+
+
+def mu_davidson(mus, mws, xs):
+    """
+    Calculate viscosity of a gas mixture.
+
+    Parameters
+    ----------
+    mus : array_like
+        Viscosity of each gas component
+    mws : array_like
+        Molecular weight of each gas component [g/mol]
+    xs : array_like
+        Mole fraction of each gas component [-]
+
+    Returns
+    -------
+    mu : float
+        Gas viscosity of the mixture. Units are same as input viscosity.
+
+    Raises
+    ------
+    here
+
+    Example
+    -------
+    here
+
+    References
+    ----------
+    here
+    """
+    a = 0.375
+    e = 2 * np.outer(mws, mws)**0.5 / np.add.outer(mws, mws)
+    f = np.sum(np.outer(xs, xs) * e**a / np.outer(mus, mus)**0.5)
+    mu_mix = 1 / f
+    return mu_mix
