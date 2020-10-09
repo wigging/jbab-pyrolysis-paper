d78a01d7dc8b11aa2ccae3fda096325bea78b50e
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2020-10-09 19:49:23 -0400

    Update results for gas mixtures properties

--- code/mix_props.py
+++ code/mix_props.py
@@ -1,6 +1,5 @@
 """
-Compare properties such as molecular weight, viscosity, and density of gas
-mixtures.
+Compare molecular weight, viscosity, and density of gas mixtures.
 """
 
 import chemics as cm
@@ -15,11 +14,11 @@ from params import press
 # Mixture properties
 # ----------------------------------------------------------------------------
 
-# gas mixtures where each item is a mixture of two gases.
-mix_gas = [('H2', 'N2'), ('CO2', 'N2'), ('CO', 'N2'), ('H2O', 'N2')]
+# gas mixtures where each item is a mixture of two gases
+mix_gas = [('N2', 'CO'), ('N2', 'CO2'), ('N2', 'H2'), ('N2', 'H2')]
 
-# weights for gas mixture where each item is fraction of the two gases [-]
-mix_wts = [(0.8, 0.2), (0.7, 0.3), (0.7, 0.3), (0.8, 0.2)]
+# mole fractions of each component in the gas mixture
+x_frac = [(0.5, 0.5), (0.5, 0.5), (0.22, 0.78), (0.02, 0.98)]
 
 mw_mix = []     # store molecular weight of each gas mixture
 mu_mix = []     # store viscosity of each gas mixture
@@ -30,12 +29,12 @@ for i in range(len(mix_gas)):
     mw2 = cm.mw(mix_gas[i][1])
     mu1 = cm.mu_gas(mix_gas[i][0], temp)
     mu2 = cm.mu_gas(mix_gas[i][1], temp)
-    wts = mix_wts[i]
+    xs = x_frac[i]
 
-    mw_mixture = cm.mw_mix((mw1, mw2), wts)
+    mw_mixture = cm.mw_mix((mw1, mw2), xs)
     mw_mix.append(mw_mixture)
 
-    mu_mixture = cm.mu_herning((mu1, mu2), (mw1, mw2), wts)
+    mu_mixture = cm.mu_herning((mu1, mu2), (mw1, mw2), xs)
     mu_mix.append(mu_mixture)
 
     rho_mixture = cm.rhog(mw_mixture, press, temp)
@@ -63,7 +62,7 @@ fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4), tight_layout=True)
 
 ax1.bar(xticks, mw_mix, align='center', color='C0')
 for i in range(len(mix_gas)):
-    ax1.text(xticks[i] - 0.3, mw_mix[i] + 0.4, f'{mix_wts[i][0]}+{mix_wts[i][1]}', fontsize=9)
+    ax1.text(xticks[i] - 0.3, mw_mix[i] + 0.4, f'{x_frac[i][0]}+{x_frac[i][1]}', fontsize=9)
 ax1.set_frame_on(False)
 ax1.set_xticks(xticks)
 ax1.set_xticklabels(xlabels)
@@ -71,7 +70,7 @@ ax1.set_ylabel('Molecular weight [g/mol]')
 
 ax2.bar(xticks, mu_mix, align='center', color='C4')
 for i in range(len(mix_gas)):
-    ax2.text(xticks[i] - 0.3, mu_mix[i] + 4, f'{mix_wts[i][0]}+{mix_wts[i][1]}', fontsize=9)
+    ax2.text(xticks[i] - 0.3, mu_mix[i] + 4, f'{x_frac[i][0]}+{x_frac[i][1]}', fontsize=9)
 ax2.set_frame_on(False)
 ax2.set_xticks(xticks)
 ax2.set_xticklabels(xlabels)
@@ -79,7 +78,7 @@ ax2.set_ylabel('Viscosity [µP]')
 
 ax3.bar(xticks, rho_mix, align='center', color='C9')
 for i in range(len(mix_gas)):
-    ax3.text(xticks[i] - 0.3, rho_mix[i] + 0.008, f'{mix_wts[i][0]}+{mix_wts[i][1]}', fontsize=9)
+    ax3.text(xticks[i] - 0.3, rho_mix[i] + 0.008, f'{x_frac[i][0]}+{x_frac[i][1]}', fontsize=9)
 ax3.set_frame_on(False)
 ax3.set_xticks(xticks)
 ax3.set_xticklabels(xlabels)
